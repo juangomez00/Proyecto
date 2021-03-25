143d8682a98872361d39d07ecd6c898691247ce0
juangomez00 <juanchoandres00@gmail.com>
2021-03-25 08:55:44 -0500

    version1.0

---
+++ Conversiones.java
@@ -0,0 +1,104 @@
+public class Conversiones{
+    public static void main(String[] args) {
+
+    byte byte1 = 127;//Declaración de variables
+    short short1 = 32767;//Declaración de variables
+    int int1 = 2147483647;//Declaración de variables
+    long long1 = 9223372036854775807l;//Declaración de variables
+    char char1 ='0';//Declaración de variables
+    float float1 = 789.675f;//Declaración de variables
+    double double1 = 90.8765;//Declaración de variables
+    boolean boolean1 = true;//Declaración de variables
+    string string1 = "Hola Mundo";
+
+
+    System.out.println("byte: "+byte1);
+    System.out.println("short: "+short1);
+    System.out.println("int: "+int1);
+    System.out.println("long: "+long1);
+    System.out.println("char: "+char1);
+    System.out.println("float: "+float1);
+    System.out.println("double: "+double1);
+    System.out.println("boolean: "+boolean1);
+    System.out.println("string: "+string1);
+
+    System.out.println("");
+    System.out.println("BYTE");
+    byte byte2 = byte1;//Declaración de variables
+    short short2 = byte1;//Declaración de variables
+    int int2 = byte1;//Declaración de variables
+    long long2 = byte1;//Declaración de variables
+    float float2 = byte1;//Declaración de variables
+    double double2 = byte1;//Declaración de variables
+    
+
+    System.out.println("byte: "+byte2);
+    System.out.println("short: "+short2);
+    System.out.println("int: "+int2);
+    System.out.println("long: "+long2);
+    System.out.println("float: "+float2);
+    System.out.println("double: "+double2);
+
+    System.out.println("");
+    System.out.println("SHORT");
+    short short3 = short1;//Declaración de variables
+    int int3 = short1;//Declaración de variables
+    long long3 = short1;//Declaración de variables
+    float float3 = short1;//Declaración de variables
+    double double3 = short1;//Declaración de variables
+    
+
+    System.out.println("short: "+short3);
+    System.out.println("int: "+int3);
+    System.out.println("long: "+long3);
+    System.out.println("float: "+float3);
+    System.out.println("double: "+double3);
+
+    System.out.println("");
+    System.out.println("CHAR");
+    char char4 = char1;
+    int int4 = char1;//Declaración de variables
+    long long4 = char1;//Declaración de variables
+    float float4 = char1;//Declaración de variables
+    double double4 = char1;//Declaración de variables
+    
+
+    System.out.println("char: "+char4);
+    System.out.println("int: "+int4);
+    System.out.println("long: "+long4);
+    System.out.println("float: "+float4);
+    System.out.println("double: "+double4);
+
+    System.out.println("");
+    System.out.println("INT");
+    int int5 = int1;//Declaración de variables
+    long long5 = int1;//Declaración de variables
+    float float5 = int1;//Declaración de variables
+    double double5 = int1;//Declaración de variables
+
+    System.out.println("int: "+int5);
+    System.out.println("long: "+long5);
+    System.out.println("float: "+float5);
+    System.out.println("double: "+double5);
+
+
+    System.out.println("");
+    System.out.println("LONG");
+    long long6 = long1;//Declaración de variables
+    float float6 = long1;//Declaración de variables
+    double double6 = long1;//Declaración de variables
+
+    System.out.println("long: "+long6);
+    System.out.println("float: "+float6);
+    System.out.println("double: "+double6);
+
+    System.out.println("");
+    System.out.println("FLOAT");
+    float float7 = float1;//Declaración de variables
+    double double7 = float1;//Declaración de variables
+
+    System.out.println("float: "+float7);
+    System.out.println("double: "+double7);
+
+    }
+}
